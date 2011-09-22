376b878645d6455710c33605b0f76d131b759875
Bas van Dijk <v.dijk.bas@gmail.com>
2011-09-22 23:27:06 +0200

    Updated copyrights

--- Control/Monad/Trans/Region/Instances/MonadsTF.hs
+++ Control/Monad/Trans/Region/Instances/MonadsTF.hs
@@ -6,7 +6,7 @@
 -------------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Monad.Trans.Region.Instances.MonadsTF
--- Copyright   :  (c) 2009 Bas van Dijk
+-- Copyright   :  (c) 2009-2011 Bas van Dijk
 -- License     :  BSD3 (see the file LICENSE)
 -- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
 --
